86841a74e82ccc561b3552aefa4927d8b777e883
QobilovOzodbek <ozodbekqobilovinfo@gmail.com>
2024-06-29 21:16:44 +0500

    add png

--- main_1.py
+++ main_1.py
@@ -1,12 +1,14 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
     win = QtWidgets.QMainWindow()
     win.setGeometry(950,300,500,500)
     win.setWindowTitle("Ozodbek Qobilov")
+    win.setWindowIcon(QIcon("icon.png"))
     win.show()
     sys.exit(app.exec_())
     
